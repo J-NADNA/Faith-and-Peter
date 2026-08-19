4089f5585aafff2ceb3610e14d933d9d3a5a18b9
ANDANJE James <okelloandanje@gmail.com>
2026-08-19 18:02:13 +0300

    Update Google Apps Script URL in config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,7 +1,7 @@
 window.WEDDING_CONFIG = {
   // IMPORTANT: Replace this with the Google Apps Script Web App URL that ends in /exec.
   // Follow README.md -> "Connect the form to Google Sheets".
-  googleAppsScriptUrl: "https://script.google.com/macros/s/AKfycbw9nsQX_8mwQksQRLPI6-8C6I2EXAP3WzhbdFZoap5yN_9dvFVR2fpTgDDBafCerHIs/exec",
+  googleAppsScriptUrl: "https://script.google.com/macros/s/AKfycbz33odHr8iMBUeaJ3dqjHmJsnjA3xMrC70W_uMx9LQQPGEz9rW6paHK8x_3b1oOFSTX/exec",
 
   weddingDate: "2026-12-05T09:00:00+03:00",
   tillNumber: "1610486",
